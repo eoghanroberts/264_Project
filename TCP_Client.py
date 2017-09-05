a6b99ef4f8f8c0810e6b6a647d7517320d3fe22c
eoghanroberts <30303258+eoghanroberts@users.noreply.github.com>
2017-09-05 19:23:56 +1200

    Updating TCP

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,34 +1,71 @@
-import socket
-import Packet
-import pickle
+#import socket
+#from struct import *
+#from Packet import packet
 
-def send():
+#test = packet(1, 1, 8, 'abduetse')
+#packet = test.setUp()
+#print(unpack('H??H{}s'.format(len(test.data)), packet))
+
+#def send():
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #channel = ('localhost', 8000) #This will need to be dynamicly set (not static)
+    ##print("Sending from Client: port = {}".format(channel[1])    
+    #sock.connect(host, port) #host and port not yet set
+    
+    #try:
+        #sentdata = 0
+        #while sentdata < message_len:
+            #sent = self.sock.send(message[sentdata:])
+            #if sent == 0:
+                #raise RuntimeError("Connection to socket broken")
+            
+        
+    #finally:
+        #print("close socket")
+        #sock.close()
+
+#send()
+
+import socket
+from struct import *
+from Packet import packet
+
+#test = packet(1, 1, 8, 'abduetse')
+#packet = test.setUp()
+def send(packet):
+    message = packet[0]
+    lenght = packet[1]
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     channel = ('localhost', 8000) #This will need to be dynamicly set (not static)
     #print("Sending from Client: port = {}".format(channel[1]))
-    test = Packet.packet(1, 1, 8, 'abduetse')
-    packet = test.setUp()
-    packet.displayPacket()
     
     
+
+
     sock.connect(channel)
-    
+
     try:
-        #send Data
-        print("sending {}".format(test.displayPacket()))
-        sock.sendall(message.encode(encoding='utf-8'))
-    
-        #Look for response
-        amount_received = 0
-        amount_expected = len(message)
-    
-        while amount_received < amount_expected:
-            data = sock.recv(518)
-            amount_received += len(data)
-            print("received {}".format(data))
+        ##send Data
+        ##print("sending {}".format(test.displayPacket()))
+        #sock.sendall(message.encode(encoding='utf-8'))
+
+        ##Look for response
+        #amount_received = 0
+        #amount_expected = len(message)
+
+        #while amount_received < amount_expected:
+            #data = sock.recv(518)
+            #amount_received += len(data)
+            #print("received {}".format(data))
+        totalsent = 0
+        while totalsent < lenght:
+            sent = self.sock.send(message[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent        
     finally:
         print("close socket")
         sock.close()
 
-send()
+send(packet(1, 1, 8, 'abduetse').setUp())
     
